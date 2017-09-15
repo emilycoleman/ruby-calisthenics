4feea3c8b4ba3f100ccd5e7ef5b91f18d9181d3c
Emily Coleman <colemanea@g.cofc.edu>
2017-09-15 21:35:08 +0000

    Finished homework two

--- lib/dessert.rb
+++ lib/dessert.rb
@@ -1,17 +1,28 @@
 class Dessert
   def initialize(name, calories)
-    # your code here
+    @name = name
+    @calories = calories
   end
+  
+  attr_accessor :name
+  attr_accessor :calories
+  
   def healthy?
-    # your code here
+    true unless @calories > 200
   end
   def delicious?
-    # your code here
+    return true
   end
 end
 
 class JellyBean < Dessert
   def initialize(flavor)
-    # your code here
+    @flavor = flavor
+    @calories = 5
+    @name = "#{flavor} jelly bean"
+  end
+  attr_accessor :flavor
+  def delicious?
+    @flavor != "licorice"
   end
 end
--- lib/fun_with_strings.rb
+++ lib/fun_with_strings.rb
@@ -1,12 +1,43 @@
 module FunWithStrings
   def palindrome?
-    # your code here
+    self.downcase!
+    self.gsub!(/[^0-9a-z]/i, '')
+    revstr = self.reverse
+    return true unless self != revstr
   end
   def count_words
-    # your code here
+    words = Hash.new(0)
+    self.downcase!
+    self.gsub!(/[^0-9a-z ]/i, '')
+    sentence = self.split
+    sentence.each do |word|
+      words[word] += 1
+    end
+    return words
   end
   def anagram_groups
-    # your code here
+    return_array = Array.new
+    self.downcase!
+    words = self.split
+    
+    words.each do |word|
+      is_matched = false
+      return_array.each do |category|
+        category.each do |head|
+          if head.chars.sort.join == word.chars.sort.join && head != word
+            is_matched = true
+            category.push(word)
+          end
+        end
+      end
+      
+      if !is_matched
+        new_array = [word]
+        return_array.push(new_array)
+      end
+    end
+    
+    return return_array
   end
 end
 
